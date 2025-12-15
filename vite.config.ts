517296104a50cdbeafeca6311f9a7f635ce74a1d
Omar Skafi <supremapix@gmail.com>
2025-12-15 18:03:20 -0300

    Add
    Site

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   publicDir: 'attached_assets',
-  base: '/',
-  appType: 'spa',
   server: {
     host: '0.0.0.0',
     port: 5000,
